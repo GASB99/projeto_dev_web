b8ef580da71475870222b2e16700376418dc4890
GASB99 <113727320+GASB99@users.noreply.github.com>
2025-06-27 00:11:20 -0300

    Delete aviaoempresa directory

--- aviaoempresa/repository/AviaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gabriel.aviaoempresa.repository;
-
-import com.gabriel.aviaoempresa.model.Aviao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AviaoRepository extends JpaRepository<Aviao, Long> {
-}
--- aviaoempresa/repository/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gabriel.aviaoempresa.repository;
-
-import com.gabriel.aviaoempresa.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-}
